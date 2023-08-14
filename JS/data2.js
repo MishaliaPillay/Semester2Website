2855752e7e8043b53145543eb81e5c81adba5e33
mish-27 <117475572+MishaliaPillay@users.noreply.github.com>
2023-08-14 09:52:01 +0200

    orignal wireframes

--- JS/data2.js
+++ JS/data2.js
@@ -1,21 +1,63 @@
 const articles = [
   {
     id: 1,
-    bigHeading:"Blog Section``",
-    headingOne: "BLOG POST 1 - CLOSE READING - OVERVIEW OF DATA VISUALISATION`` ",
-    paraOne: "the WETt Codbase",
-    headingTwo:` <a href="blogs.html" class="opened"> Blogs</a>
-    </li>`,
+    bigHeading:"Wireframes",
+    headingOne: "Orginal Wireframes",
+    paraOne: `<figure>
+    <img
+      src="./Images/HomePagept1og.png"
+      alt="This is orignal homepage wireframe"
+      id="styleImg2"
+    />
+    <figcaption>This is the orignal homepage wireframe. The homepage is envisioned to be one long page that gives the users an overview of what can be  found on the site .Thus the images below will follow in sequence after this image.
+    </figcaption>
+  </figure><figure>
+  <img
+    src="./Images/HomePagept2og.png"
+    alt="Design and Blog section orginal wireframe for homepage."
+    id="styleImg2"
+  />
+  <figcaption>This is where the Blog and Design section overview wil be. Within the two boxes will be descriptions of those Pages. </figcaption>
+</figure><figure>
+<img
+  src="./Images/HomePagept3og.png"
+  alt="Data-Art and Data-Visualisation sections , with a back to the top button on homapage."
+  id="styleImg2"
+/>
+<figcaption>This shows the Data-Art and Data-Visualisation sections these will also contain descriptions . This is the end of the homepage and there will be a back to the top button. </figcaption>
+</figure>`,
+    headingTwo:` `,
     paraTwo: "", 
-    headingThree: "what are the react team principles",
-    paraThree: "the WETt Codbase",
+    headingThree: "Annotated Wireframes",
+    paraThree: `<figure>
+    <img
+      src="./Images/Figure3.png"
+      alt="Figure 3: Bar graph comparison"
+      id="styleImg2"
+    />
+    <figcaption>Figure 3: Bar graph comparison</figcaption>
+  </figure><figure>
+  <img
+    src="./Images/Figure3.png"
+    alt="Figure 3: Bar graph comparison"
+    id="styleImg2"
+  />
+  <figcaption>Figure 3: Bar graph comparison</figcaption>
+</figure><figure>
+<img
+  src="./Images/Figure3.png"
+  alt="Figure 3: Bar graph comparison"
+  id="styleImg2"
+/>
+<figcaption>Figure 3: Bar graph comparison</figcaption>
+</figure>`,
    headingFour: "what are the react team principles",
     paraFour: "the WETt Codbase",
    
   },
   {
     id: 2,
-    bigHeading:"Blog Section",
+    bigHeading:"IXD Process",
     headingOne: "goodby, clean code",
     paraOne: "the WETt Codbase",
     headingTwo: "what are the react team principles",
