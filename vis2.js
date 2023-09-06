49e451e8853810415d1c500c962153cdc814ac77
mish-27 <117475572+MishaliaPillay@users.noreply.github.com>
2023-09-06 17:02:19 +0200

    First plot point !!

--- vis2.js
+++ vis2.js
@@ -1,99 +1,65 @@
-// Define the API URL
-const apiUrl = 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-08-07&end_date=2023-08-08&api_key=8Vtxr88AbUfI12VOV2uXnn06djnja0v4eLhGN1sA';
+//DIMENSIONS
+let height =600,
+width=600,
+margin=80;
 
-async function fetchData() {
-  try {
-    const response = await fetch(apiUrl);
 
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
 
-    const data = await response.json();
-
-    return data.near_earth_objects; // Adjust this based on the structure of your data
-  } catch (error) {
-    // Handle any errors that occurred during the fetch
-    console.error('Fetch error:', error);
-    throw error; // Re-throw the error for further handling if needed
-  }
-}
 
 async function createScatterPlot() {
   try {
-    const asteroidData = await fetchData();
+    let asteroidData = await fetchAstData();
 
-    // Your D3.js code here
-    const xScale = d3.scaleLinear()
-      .domain([0, d3.max(asteroidData, d => d.near_earth_objects["2023-08-07"][0].close_approach_data[0].miss_distance["kilometers"]
-        )])
+    // Select the SVG container element
+    let svg = d3.select('section')
+      .append('svg')
+      .attr('height',height+ margin +margin)
+      .attr("width", width+ margin +margin )
+      .append('g')
+      .attr("transform",  `translate(${margin}, ${margin})`);
+     // near_earth_objects["2023-08-07"][0].close_approach_data[0].close_approach_date
+    // Parse dates and miss distances\
+  
+    let parsedData = asteroidData.near_earth_objects["2023-08-07"].map((asteroid) => {
+      let closestApproach = asteroidData.near_earth_objects["2023-08-07"][0].close_approach_data[0];
+      return {
+        date: new Date(closestApproach.close_approach_date),
+        missDistance: parseFloat(closestApproach.miss_distance.kilometers),
+      };
+    });
+    // Set up scales
+    let xScale = d3.scaleTime()
+      .domain([d3.min(parsedData, d => d.date), d3.max(parsedData, d => d.date)])
       .range([0, width]);
 
-    const yScale = d3.scaleLinear()
-      .domain([0, d3.max(asteroidData, d => d.diameter)])
+    let yScale = d3.scaleLinear()
+      .domain([0, d3.max(parsedData, d => d.missDistance)])
       .range([height, 0]);
 
-    // ... rest of your D3.js code ...
+    // Create circles for data points
+    svg.selectAll('circle')
+      .data(parsedData)
+      .enter()
+      .append('circle')
+      .attr('cx', d => xScale(d.date))
+      .attr('cy', d => yScale(d.missDistance))
+      .attr('r', 5)
+      .style('fill', 'red');
+
+    // Add X and Y axes
+    let xAxis = d3.axisBottom(xScale);
+    let yAxis = d3.axisLeft(yScale);
+
+    svg.append('g')
+      .attr('transform', `translate(0, ${height})`)
+      .call(xAxis);
+
+    svg.append('g')
+      .call(yAxis);
   } catch (error) {
-    // Handle any errors that occurred during data retrieval or D3.js code execution
     console.error('Error:', error);
   }
 }
 
 // Call the function to create the scatter plot
 createScatterPlot();
-
-
-
-
-
-
-
-
-const width = 600; // Set the width of the SVG container
-const height = 400; // Set the height of the SVG container
-
-const svg = d3.select('body') // Select the HTML element to contain the SVG
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height);
-  
-  const xScale = d3.scaleLinear()
-  .domain([0, d3.max(asteroidData, d => d.near_earth_objects["2023-08-07"][0].close_approach_data[0].miss_distance["kilometers"]
-    )]) // Adjust the domain as needed
-  .range([0, width]);
-
-const yScale = d3.scaleLinear()
-  .domain([0, d3.max(asteroidData, d => d.diameter)]) // Adjust the domain as needed
-  .range([height, 0]);
-  svg.selectAll('circle')
-  .data(asteroidData)
-  .enter()
-  .append('circle')
-  .attr('cx', d => xScale(d.distance)) // X-coordinate based on distance
-  .attr('cy', d => yScale(d.diameter)) // Y-coordinate based on diameter
-  .attr('r', 5) // Set the circle radius
-  .style('fill', 'blue'); // Set the circle fill color
-  const xAxis = d3.axisBottom(xScale);
-const yAxis = d3.axisLeft(yScale);
-
-svg.append('g')
-  .attr('transform', `translate(0, ${height})`)
-  .call(xAxis);
-
-svg.append('g')
-  .call(yAxis);
-
-// Add axis labels
-svg.append('text')
-  .attr('x', width / 2)
-  .attr('y', height + 30)
-  .style('text-anchor', 'middle')
-  .text('Distance');
-
-svg.append('text')
-  .attr('transform', 'rotate(-90)')
-  .attr('x', -height / 2)
-  .attr('y', -40)
-  .style('text-anchor', 'middle')
-  .text('Diameter');
