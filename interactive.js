fb47398e0a72cf37cea34c7f86eeb8d86143824f
MishaliaPillay <117475572+MishaliaPillay@users.noreply.github.com>
2023-10-09 10:08:44 +0200

    10/09

--- interactive.js
+++ interactive.js
@@ -1,11 +1,11 @@
-let heighttt = window.innerHeight,
-    widthhh = window.innerWidth;
+let height = window.innerHeight,
+    width = window.innerWidth;
 
 // CREATE SVG
 let svg = d3.select("#rootttt")
     .append("svg")
-    .attr("height", heighttt)
-    .attr("width", widthhh)
+    .attr("height", height)
+    .attr("width", width)
     .style("background-color", '#00a0a949');
 const TOLERANCE = 0.0001
 // Sample API call (replace with your data fetching logic)
@@ -20,15 +20,15 @@ async function fetchData() {
       
 
     // FORCE SIMULATION
-    let forceXcombine = d3.forceX(widthhh / 2).strength(0.04);
+    let forceXcombine = d3.forceX(width / 2).strength(0.04);
     let forceXsplit = d3.forceX(function (d) {
-    return d.size <= 20 ? 100 : widthhh - 100;
+    return d.size <= 20 ? 100 : width - 100;
 }).strength(0.4);
-    let forceY = d3.forceY(heighttt / 2).strength(0.04);
+    let forceY = d3.forceY(height / 2).strength(0.04);
     let forceCollide = d3.forceCollide((d) => rScale(d.size)*1);
     let simulation = d3.forceSimulation(transformedData)
-    .force("forceX", d3.forceX(widthhh / 2).strength(0.04))
-    .force("forceY", d3.forceY(heighttt / 2).strength(0.04))
+    .force("forceX", d3.forceX(width/ 2).strength(0.04))
+    .force("forceY", d3.forceY(height / 2).strength(0.04))
     .force("forceCollide", d3.forceCollide().radius(d => rScale(d.size) + 1))
     .alphaDecay(0)
     .alphaMin(0.1);
@@ -56,23 +56,23 @@ async function fetchData() {
     })
     d3.select("#split").on("click", () => { removeArrow();
       svg.selectAll(".date-text").remove();
-      simulation.force("forceX", d3.forceX(d => d.size <= 20 ? 100 : widthhh - 100).strength(0.4)).alpha(0.5).restart();
+      simulation.force("forceX", d3.forceX(d => d.size <= 20 ? 100 : width - 100).strength(0.4)).alpha(0.5).restart();
     });
   
     d3.select("#combine").on("click", () => {  removeArrow();
       svg.selectAll(".date-text").remove();
-      simulation.force("forceX", d3.forceX(widthhh / 2).strength(0.04)).alpha(0.5).restart();
+      simulation.force("forceX", d3.forceX(width / 2).strength(0.04)).alpha(0.5).restart();
     });
     d3.select("#separate-by-date-button").on("click", () => { removeArrow();
       const dateScale = d3.scaleLinear()
         .domain([d3.min(transformedData, d => d.date), d3.max(transformedData, d => d.date)])
-        .range([100, widthhh - 100]);
+        .range([100, width - 100]);
         svg.selectAll(".date-text")
         .data([d3.min(transformedData, d => d.date), d3.max(transformedData, d => d.date)])
         .enter()
         .append("text")
         .attr("x", d => dateScale(d)) // Use the dateScale to position the text
-        .attr("y", heighttt - 30) // Y-coordinate just above the bottom of the SVG
+        .attr("y", height - 30) // Y-coordinate just above the bottom of the SVG
         .attr("text-anchor", "middle")
         .attr("fill", "black")
         .text(d => d3.timeFormat('%b %d')(d))
@@ -87,7 +87,7 @@ async function fetchData() {
       // Calculate the x-scale based on sorted speeds
       const xScale = d3.scaleLinear()
         .domain([0, transformedData.length - 1])
-        .range([widthhh / 4, (3 * widthhh) / 4]);
+        .range([width / 4, (3 * width) / 4]);
     
       // Apply forces to arrange bubbles based on speed
       simulation
@@ -122,17 +122,17 @@ async function fetchData() {
   
     // Add the arrow below the bubbles
     svg.append("line")
-      .attr("x1", widthhh / 4) // Start position (left side)
-      .attr("x2", (3 * widthhh) / 4) // End position (right side)
-      .attr("y1", heighttt - 20) // Y-coordinate of the line (just above the bottom of the SVG)
-      .attr("y2", heighttt - 20) // Y-coordinate of the line (just above the bottom of the SVG)
+      .attr("x1", width / 4) // Start position (left side)
+      .attr("x2", (3 * width) / 4) // End position (right side)
+      .attr("y1", height - 20) // Y-coordinate of the line (just above the bottom of the SVG)
+      .attr("y2", height - 20) // Y-coordinate of the line (just above the bottom of the SVG)
       .attr("marker-end", "url(#arrow)") // Attach the arrow marker
       .attr("stroke", "black")
-      .attr("stroke-widthhh", 2)
+      .attr("stroke-width", 2)
       .attr("class", "arrow-line"); // Add a class to the arrow line for easy removal
       svg.append("text")
-      .attr("x", (3 * widthhh) / 4 -5) // X-coordinate at the end of the arrow head (right side)
-      .attr("y", heighttt - 30) // Y-coordinate just above the bottom of the SVG
+      .attr("x", (3 * width) / 4 -5) // X-coordinate at the end of the arrow head (right side)
+      .attr("y", height - 30) // Y-coordinate just above the bottom of the SVG
       .attr("text-anchor", "end")
       .attr("fill", "black")
       .text("Highest Speed")
@@ -149,7 +149,7 @@ let tooltipppp = d3.select('#rootttt')
     .append('p')
     .attr("stroke", "blue", "2px")
     .style('opacity', 0)
-    .style("widthhh", "180px")
+    .style("width", "180px")
     .style("border-radius", "5px")
     .attr("font-size", "8px")
     .style('padding', "20px")
