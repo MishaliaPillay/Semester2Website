34537bdae895eac876115054a459d87768f4659a
MishaliaPillay <2435450@students.wits.ac.za>
2023-11-14 18:47:26 +0200

    11/14

--- JS/data4.js
+++ JS/data4.js
@@ -5,22 +5,8 @@ let exam= [
       `,
       headingOne: "What more can be extracted and explored?",
       paraOne: "For my Exam Data Art, I decided to expand on the data from my previous Data Art. The Nasa Api link that I use allows us to browse through the Nasa database on asteroids. This link however only allows us to see the data for 20 asteroids at a time. I have created a slider that allows the user to choose different pages on the NASA website thus different sets of 20 asteroids are shown each time the slider is moved. ",
-      image:`<figure>
-      <img
-        src="./Images/dashed1.png"
-        alt="dashed lines image"
-        id="imgg2"
-      />
-      <figcaption>Dashed lines from size scale.</figcaption>
-      </figure><figure>
-      <img
-        src="./Images/dashed2.png"
-        alt="dashed lines image"
-        id="imgg2"
-      />
-      <figcaption>Dashed lines from Circular Bar plot.</figcaption>
-      </figure>`,
-      paraTwo:`id="Design"`, 
+      image:``,
+      paraTwo:`id="Exam"`, 
       headingThree: "What more can be said about your data? ",
       paraThree: "The only information that was excluded from my previous data art was the velocities of the asteroids. For my Exam data art, I wanted to explore this visual and make more use of animations. The Asteroid data is arranged by date on the y-axis and that acts as a starting point, the lines move across the screen towards the planet on the right of the screen. This simulates the asteroids moving towards planets, as the rate at which the lines move is dependent on their velocities. ",
      
@@ -30,14 +16,7 @@ let exam= [
       bigHeading:``,
       headingOne: "Interactivity",
       paraOne: "Each time the slider is moved new data is loaded and when the user clicks play they are able to visualise the speed of the asteroids in that data set. The range of the speed of movement of the lines changes with the data, I have added another interactive slider, which allows the user to speed up and slow down the movement of the lines while maintaining the integrity of the data. ",
-      image: `<figure>
-      <img
-        src="./Images/dashed1.png"
-        alt="Size scale image"
-        id="imgg2"
-      />
-      <figcaption>Circles from the Size Scale.</figcaption>
-      </figure>`,
+      image: ``,
       paraTwo: `id="asteClassi"`,
       headingThree: "Observation",
       paraThree: "The data visualisation displays well over 900 data points at a time, and through analysis of the data I found that the velocities of the asteroids have an estimated range of 3 kms to 32 kms . I also found that the data is spread almost evenly and to convey this to the user I created two interactive elements in the form of buttons which show asteroids that are faster than 15 kms and asteroids that are slower than 15 kms. To stick to the convenience of my website I decided to change the opacity of the asteroids in the background when the buttons are clicked. ",
@@ -48,14 +27,7 @@ let exam= [
       bigHeading:``,
       headingOne: "", 
       paraOne: "When the user clicks the play button the simulation starts and replays a few times. This allows the user to fiddle with the speed slider and the filter buttons. I also found it useful to add a pause button which allows all the asteroid data lines to reach their end point on the right of the screen this makes it easier for the user to hover over the lines and find out more information that is displayed on the tooltip. ",
-      image: `<figure>
-      <img
-        src="./Images/bars.png"
-        alt="Circular bar plot bars image"
-        id="imgg2"
-      />
-      <figcaption>Circular bar plots , bars with differnet colurs with the same width.</figcaption>
-      </figure>`,
+      image: ``,
       paraTwo: `id="asteName"`,
       headingThree: "How have communication strategies, user experience and interaction been used to enhance or subvert meaning in the artwork? ",
       paraThree: "As a form of visual communication, I decided to highlight the line that the user hovers over with a white line that is slightly thicker than the lines of the data points. This is an act of clarity and it is very clear to the user that this is the data point that they are hovering over and that the tooltips show the corresponding information for that point. ",
@@ -66,21 +38,7 @@ let exam= [
       bigHeading:``,
       headingOne: "Can artistic strategies be used to compare and contrast data and interpretation? ",
       paraOne: "I made the lines of the data points dashed, this was done since the dashed lines acted as a loading visual. The dashed lines helped separate the data visually and made it look cleaner as there was more to be seen when the user could distinguish between the different lines. ",
-      image: `<figure>
-      <img
-        src="./Images/tooltip.png"
-        alt="tooltip  image"
-        id="imgg2"
-      />
-      <figcaption>Tooltip as user hovers over a bar,on Circular bar plot.</figcaption>
-      </figure><figure>
-      <img
-        src="./Images/btns.png"
-        alt="buttons image"
-        id="imgg2"
-      />
-      <figcaption>Image of the buttons.</figcaption>
-      </figure>`,
+      image: ``,
       paraTwo: "",
       headingThree: "",
       paraThree: "I decided to add a gradient colour to the lines that go from teal to red. This is because the lines start from a safe distance away from the planets and as they reach closer it becomes more of a danger to the planet. The teal is a cool colour suggesting emotions of safety and red is a symbol of danger. ",
@@ -90,15 +48,8 @@ let exam= [
         bigHeading:``,
         headingOne: "How does this art relate to your other visuals?",
         paraOne: "The emotion for this data art is fascination, I wanted to entertain and excite the user by showing them an animated visual that shows actual data on asteroids. All of my other data visualisations show the complexity and vastness of space and asteroid entities that Earth encounters. This data art simulates the movement of these asteroids towards Earth and other planets.",
-        image: `<figure>
-        <img
-          src="./Images/scale.png"
-          alt="14-17 size scale image"
-          id="imgg2"
-        />
-        <figcaption>This image shows the asteroids between sizes 14-17h.</figcaption>
-        </figure>`,
-        paraTwo:`id="Observe"`,
+        image: ``,
+        paraTwo:``,
         headingThree: "",
         paraThree: "  This data art is fascinating since it captures and holds the attention of the viewer by stimulating them mentally and visually as they take in this data and all the possible meanings that it could suggest.",
         
